aa89d8b556a9f6110c47762a76580339d96a41e8
haszari <haszari@cartoonbeats.com>
2017-07-16 11:14:33 +1200

    bulma styling and menu header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
 import {Link, IndexLink} from 'react-router'
 
+import logoImage from '../../assets/images/20170620-vogelstreetparty-2017-logo-crop.png'
+
+
 export default class Header extends React.Component {
-  render() {
-    return (
-      <nav>
-        <Link to="/">Radness Tech Inc.</Link>
-      </nav>
-    )
-  }
+   render() {
+      let logoImageStyle = {
+         height: '25px',
+         paddingRight: '8px',
+      };
+
+      return (
+         <section className="header">
+            <div className="tabs is-fullwidth">
+               <ul>
+                  <li>
+                     <Link className="" to="/">
+                        <img src={logoImage} style={logoImageStyle} alt="Ideas &amp; Incubation"></img>
+                        Vogel Street Party 2017 
+                     </Link>
+                  </li>
+                  <li>
+                     <Link className="" to="/contact">
+                        Events
+                     </Link>
+                  </li>
+                  <li>
+                     <Link className="" to="/contact">
+                        Contact
+                     </Link>
+                  </li>
+               </ul>
+            </div>
+         </section>
+      )
+   }
 }
