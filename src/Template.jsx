04f4a36ed19834e5c9853040f7a87fcafa7f934d
haszari <haszari@cartoonbeats.com>
2017-07-16 11:45:59 +1200

    footer social links (pull in fontawesome from CDN)

--- src/Template.jsx
+++ src/Template.jsx
@@ -6,6 +6,7 @@ export default class Template extends React.Component {
       <html>
         <head>
           <link rel="stylesheet" href="/styles.css"/>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
         </head>
         <body>
             <div id='outlet' className="container">
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import {Link, IndexLink} from 'react-router'
 
+
+
 export default class Footer extends React.Component {
   render() {
     return (
-      <footer>
-        <div>
-          <div>
-            <IndexLink to="/">Home</IndexLink>
-            <Link to="/contact">Contact</Link>
-          </div>
-          <div >Site by Radness Tech 2016</div>
-        </div>
+      <footer className="footer">
+         <div className="container">
+            <div className="content has-text-centered">
+               <div className="social-links">
+                  <a href="https://www.facebook.com/vogelstreetparty">
+                     <span className="icon"><i className="fa fa-facebook"></i></span>
+                     <span className="is-hidden-mobile">vogelstreetparty</span>
+                  </a>
+                  <a href="https://www.instagram.com/vogelstparty/">
+                     <span className="icon"><i className="fa fa-instagram"></i></span>
+                     <span className="is-hidden-mobile">vogelstparty</span>
+                  </a>
+                  <a href="https://twitter.com/vogelstparty">
+                     <span className="icon"><i className="fa fa-twitter"></i></span>
+                     <span className="is-hidden-mobile">vogelstparty</span>
+                  </a>
+               </div>
+            </div>
+         </div>
       </footer>
     )
   }
