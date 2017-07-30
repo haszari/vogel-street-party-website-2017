793a6042943a11d25b777a4acf8bc59b11eee044
haszari <haszari@cartoonbeats.com>
2017-07-30 13:23:43 +1200

    show events in a grid with time info

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import moment from 'moment'
+
 import eventData from '../../data/events'
 
+
 const introSection = (
-   <section className="main-content section">
+   <section className="main-content section intro">
       <div className="container">
          <div className="content">
             <h1 className="title">Vogel Street Party</h1>
@@ -14,30 +17,51 @@ const introSection = (
    </section>
 );
 
-const eventSection = function(key, title, copy) {
-   return (
-      <section key={key} className="main-content section">
-         <div className="container">
-            <div className="content">
-               <h1 className="title">{title}</h1>
-               <div>
-                  {copy}
-               </div>
-            </div>
+const eventSection = function(key, title, copy, startTime, endTime) {
+   let dataFormat = 'h:mm p';
+   let displayFormat = 'h:mm';
+   let timeslot = {
+      start: moment(startTime, dataFormat),
+      end: moment(endTime, dataFormat),
+   };
+   let timeInfo = '';
+   let emdash = '\u2014';
+   if (timeslot.start.isValid()) {
+      let endInfo = '';
+      if (timeslot.end.isValid())
+         endInfo = (<span> {emdash}<span className="timeslot-end">{timeslot.end.format(displayFormat)}</span></span>);
+      timeInfo = (
+         <div className="timeslot">
+            <span className="timeslot-start">{timeslot.start.format(displayFormat)}</span>{endInfo}
          </div>
-      </section>
+      );
+   }
+   return (
+      <div key={key} className="column is-4">
+         <div className="box">
+            <h1 className="title">{title}</h1>
+            <div>
+               {copy}
+            </div>
+            {timeInfo}
+         </div>
+      </div>
    );
 }
 
 export default class Home extends React.Component {
    render() {
       let events = eventData.map((event, index) => {
-         return eventSection(index, event.title, event.blurb);
+         return eventSection(index, event.title, event.blurb, event.start, event.end);
       });
       return (
          <div> 
-         { introSection }
-         { events } 
+            { introSection }
+            <section className="main-content section events">
+               <div className="columns is-multiline">
+                  { events } 
+               </div>
+            </section>
          </div> 
       )
    }
