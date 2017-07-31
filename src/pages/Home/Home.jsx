b8aa765d199833effa992e4d7fadb24fddf753e8
haszari <haszari@cartoonbeats.com>
2017-07-31 20:46:20 +1200

    woops put back intro section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,22 @@ import moment from 'moment'
 
 import EventGuide from '../../components/event-guide.jsx'
 
+const introSection = (
+   <section className="main-content section intro">
+      <div className="container">
+         <div className="content intro">
+            <h1 className="title">Vogel Street Party 2017</h1>
+            <div>
+               <p>This year the party is all about <b>Ideas and Incubation</b>.</p>
+               <p>Saturday the 14th October, Vogel Street, Dunedin</p>
+               <p>Free &amp; all-ages!</p>
+            </div>
+         </div>
+      </div>
+   </section>
+);
+
+
 export default class Home extends React.Component {
    render() {
       let categories = ['building', 'performance', 'installation', 'activity'];
