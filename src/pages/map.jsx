5375604d483e0886a9a6669a1e1bdc51a61862da
haszari <haszari@cartoonbeats.com>
2017-08-01 20:25:03 +1200

    map page

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -1,12 +1,14 @@
 
 import React from 'react'
 
+import aerialImage from '../assets/images/vogel-street-aerial.jpg'
+
 export default function Map() {
    return (
       <section className="main-content section map">
          <div className="container">
             <div className="content map">
-               <h1 className="title">MAP</h1>
+               <img src={aerialImage} alt="Vogel Street Party Map"></img>
             </div>
          </div>
       </section>
