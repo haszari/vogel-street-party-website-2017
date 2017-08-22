d32d865dd059eee61e5173d127cf319ef169ae23
haszari <haszari@cartoonbeats.com>
2017-08-22 22:12:44 +1200

    expressions of interest form is linked on contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,16 +3,18 @@ import React from 'react'
 export default class Contact extends React.Component {
    render() {
       return (
-         <section className="main-content section">
-            <div className="container">
-               <div className="content">
-                  <h1>Coming soon ..</h1>
-                  <div>
-                     <p>Watch this space for how to get involved or support the event.</p>
+         <div className="contact background"> 
+            <section className="main-content">
+               <div className="container has-text-centered">
+                  <div className="content">
+                     <h1>Expressions of Interest</h1>
+                     <div>
+                        <p><b><a href="https://goo.gl/forms/uoh37DYwzsPe7AKW2">Click here</a></b> and let us know how you'd like to be involved!</p>
+                     </div>
                   </div>
                </div>
-            </div>
-         </section>
+            </section>
+         </div>
       )
    }
 }
