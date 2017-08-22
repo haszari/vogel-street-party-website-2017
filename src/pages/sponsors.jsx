be9c2053c21941734325db2d6a76f73e13477dd4
haszari <haszari@cartoonbeats.com>
2017-08-22 20:29:31 +1200

    get sponsor logos rendering + remove unused "images"

--- src/pages/sponsors.jsx
+++ src/pages/sponsors.jsx
@@ -5,17 +5,20 @@ import sponsorData from '../data/sponsors'
 
 const sponsorBox = function(key, sponsorInfo) {
    let classes = "sponsor-" + sponsorInfo.level;
+   let image = require(`../assets/images/sponsors/${sponsorInfo.image}`);
+   // let title = (<h1 className="title event-title">{sponsorInfo.name}</h1>);
+
    return (
       <div key={key} className="column is-4">
-         <div className={classes}>
-            <h1 className="title event-title">{sponsorInfo.name}</h1>
+         <div className={classes} >
+            <img src={image} alt={sponsorInfo.name}></img>
          </div>
       </div>
    );
 }
 export default function Sponsors() {
    let sponsorLogos = sponsorData.map((item, index) => {
-      console.log({index, item});
+      // console.log({index, item});
       return sponsorBox(index, item);
    });
 
